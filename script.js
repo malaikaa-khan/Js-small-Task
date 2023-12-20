756b3c3392cad7d98f038dab3ba23587d17fd249
malaikaa-khan <148899222+malaikaa-khan@users.noreply.github.com>
2023-12-20 10:35:06 +0500

    Added Files

---
+++ script.js
@@ -0,0 +1,39 @@
+let container=document.getElementById("container");
+
+
+
+
+function greenBtn(){
+    container.style.backgroundColor = "Green";
+}
+
+function roseBtn(){
+    container.style.backgroundColor = " #ffc3a0";
+}
+
+function purpleBtn(){
+    container.style.backgroundColor = " #753a88";
+}
+
+function blueBtn(){
+    container.style.backgroundColor = "#6dd5ed";
+}
+
+
+/*-----Circle buttons-----*/
+
+function redBtn(){
+    container.style.backgroundColor = "red";
+}
+
+function skyBtn(){
+    container.style.backgroundColor = " rgb(0, 247, 255)";
+}
+
+function pinkBtn(){
+    container.style.backgroundColor = " pink";
+}
+
+function brownBtn(){
+    container.style.backgroundColor = "brown";
+}
